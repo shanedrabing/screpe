5995a5f2b00d0f273f0e510cbafe46ce27cdfa01
Shane Drabing <shane.drabing@gmail.com>
2022-11-27 15:37:57 -0700

    new gitignore, nabbed the name on pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="screpe",
+    version="0.0.0",
+    author="Shane Drabing",
+    author_email="shane.drabing@gmail.com",
+    packages=setuptools.find_packages(),
+    url="https://github.com/shanedrabing/screpe",
+    description="High-level Python web scraping.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    data_files=[
+        ("", ["LICENSE"])
+    ],
+    install_requires=[
+        "requests", "bs4"
+    ]
+)
