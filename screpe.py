eb9675e99f4ef39b621928812bb80d1d3a453c59
Shane Drabing <shane.drabing@gmail.com>
2022-11-27 15:42:58 -0700

    testing import time

---
+++ screpe.py
@@ -0,0 +1,31 @@
+__author__ = "Shane Drabing"
+__license__ = "MIT"
+__version__ = "0.0.0"
+__email__ = "shane.drabing@gmail.com"
+
+
+# IMPORTS
+
+
+print("bs4")
+import bs4
+print("requests")
+import requests
+print("selenium")
+import selenium
+print("pandas")
+import pandas
+print("done")
+
+
+# CONSTANTS
+
+
+# FUNCTIONS
+
+
+# SCRIPT
+
+
+if __name__ == "__main__":
+    pass
